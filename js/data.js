eb006add3db96784bd6bfb9a539fa89e1507223e
sarveswaran <sarveswaran2003@gmail.com>
2026-07-04 15:14:16 +0530

    added links

--- js/data.js
+++ js/data.js
@@ -10,7 +10,8 @@ const LINKS = [
   {id:'l1',name:'Thenmozhi',capital:'₹70K',duration:'8 Months',link:'https://profile.mynt.in/dailypnl?ucode=6zCsvBLyNBlfYFyiGGJ2eqcG'},
   {id:'l2',name:'Sruthi',capital:'₹1L',duration:'8 Months',link:'https://profile.mynt.in/dailypnl?ucode=KcYhqkt3hMPTDAQmQEaUKU4x'},
   {id:'l3',name:'Kani',capital:'₹2.3L',duration:'7 Months',link:'https://profile.mynt.in/dailypnl?ucode=u5r7rp0Hcr11qrLZQnG75XHd'},
-  {id:'l4',name:'Ravi',capital:'₹1L',duration:'1 Month',link:'https://console.zerodha.com/verified/767c4a37'}
+  {id:'l4',name:'Ravi',capital:'₹1L',duration:'1 Month',link:'https://console.zerodha.com/verified/767c4a37'},
+  {id:'l5',name:'Rajan',capital:'₹1L',duration:'3 Month',link:'https://console.zerodha.com/verified/c91d74d6'}
 ];
 
 /*
@@ -22,5 +23,8 @@ const LINKS = [
   {id:'s2', name:'Someone', capital:'₹1L', duration:'6 Months', image:'assets/screenshots/someone-1.png'}
 */
 const SNAPS = [
-  {id:'s1',name:'Thamaraikannan',capital:'₹1L',duration:'1 Year',image:'assets/screenshots/thamaraikannan-1.png'}
-];
+  { id: 's1', name: 'Thamaraikannan', capital: '₹1L', duration: '1 Year', image: 'assets/screenshots/thamaraikannan1.jpg' },
+  { id: 's2', name: 'Mathi', capital: '₹70k', duration: '2 Months', image: 'assets/screenshots/mathi1.jpg' },
+  { id: 's3', name: 'Srinivas', capital: '₹9.6L', duration: '4 Months', image: 'assets/screenshots/Srinivas1.jpg' },
+  { id: 's4', name: 'Thirumaran', capital: '₹2.9L', duration: '10 Months', image: 'assets/screenshots/Thirumaran1.jpg' },
+]
