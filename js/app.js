98832712066086a54c111c3f59a83f476f078bcf
sarveswaran <sarveswaran2003@gmail.com>
2026-07-04 19:12:23 +0530

    dark theme

--- js/app.js
+++ js/app.js
@@ -218,5 +218,5 @@ function toast(msg){
 }
 
 /* ---------- BOOT ---------- */
-applyTheme(document.documentElement.getAttribute('data-theme')||'light');
+applyTheme(document.documentElement.getAttribute('data-theme')||'dark');
 render();
